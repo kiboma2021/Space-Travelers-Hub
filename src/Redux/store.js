3c1cfc67a593c8747a8cec2d71dc0ad3cf9717b6
officialesse <uzomaesse@gmail.com>
2022-07-21 22:08:03 +0100

    Feat: PERSISTED STATE

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers, } from '@reduxjs/toolkit';
+import storage  from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import rocketReducer from './rocket/rocket'
 import missionReducer from './mission/mission'
 
-export default configureStore({
-  reducer:{
-    rocket:rocketReducer,
-    mission:missionReducer
-  }
+const rootReducer = combineReducers({ 
+  rocket:rocketReducer,
+  mission:missionReducer
+
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const   store = configureStore({
+  reducer:persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: false,
+        }),
+})
+export const persistor = persistStore(store)
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './Redux/store';
+import {store,persistor} from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>  
     </Provider>
   </React.StrictMode>
 );
