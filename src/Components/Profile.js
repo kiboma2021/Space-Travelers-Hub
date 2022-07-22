855de53bc0ac039e5bd3e41009dadb106917ff18
Ben Kiboma <omayioben@gmail.com>
2022-07-22 21:15:07 +0300

    Style profile page components

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,12 +1,12 @@
 import React from "react";
 import MissionCard from "./profile/MissionCard";
 import RocketCard from "./profile/RocketCard";
-
+import '../Styles/Profile.css'
 
 const Profile = () => {
 
   return (
-    <div>
+    <div className="profile-page">
     <MissionCard/>
     <RocketCard/>
     </div>
